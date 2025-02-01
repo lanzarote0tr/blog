562be450935423de8a4e9f562b45e25e4d60c773
EthanWonCho <realethanwoncho@gmail.com>
2025-02-01 14:43:47 +0900

    database connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const pool = require('../utils/connectdb');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  try {
+    const [result] = await pool.query("SELECT * FROM posts");
+    res.render('index', { title: 'Express', posts: result });
+  } catch(err) {
+    console.error('Query Error: ', err);
+    next(createError(500));
+  }
 });
 
 module.exports = router;
