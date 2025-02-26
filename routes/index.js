9b30fb8a9e1697c62b4e9e6ebd44b94ef79c1397
EthanWonCho <realethanwoncho@gmail.com>
2025-02-27 08:22:30 +0900

    minor edit

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 var router = express.Router();
 
-import pool from '../utils/connectdb.js';
+// import pool from '../utils/connectdb.js';
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  try {
-    const [result] = await pool.query("SELECT * FROM posts");
-    res.render('index', { title: 'Express', posts: result });
-  } catch(err) {
-    next(err);
-  }
+  // try {
+  //   const [result] = await pool.query("SELECT * FROM posts");
+  //   res.render('index', { title: 'Express', posts: result });
+  // } catch(err) {
+  //   next(err);
+  // }
+  res.status(200).send("김보민 바보");
 });
 
 export default router;
