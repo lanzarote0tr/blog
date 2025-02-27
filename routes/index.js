cb307f84e90e7511f4de7ca07d6ddeb97f37843b
EthanWonCho <realethanwoncho@gmail.com>
2025-02-27 19:52:07 +0900

    add db connection

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 import express from 'express';
 var router = express.Router();
 
-// import pool from '../utils/connectdb.js';
+import pool from '../utils/connectdb.js';
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  // try {
-  //   const [result] = await pool.query("SELECT * FROM posts");
-  //   res.render('index', { title: 'Express', posts: result });
-  // } catch(err) {
-  //   next(err);
-  // }
-  res.status(200).send("김보민천재");
+  try {
+    const [result] = await pool.query("SELECT * FROM posts");
+    res.render('index', { title: 'Express', posts: result });
+  } catch(err) {
+    next(err);
+  }
 });
 
 export default router;
