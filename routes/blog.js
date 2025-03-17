d6d034628f45265ccf7c079556ce5d4a0fc8bd2b
EthanWonCho <realethanwoncho@gmail.com>
2025-03-17 22:23:10 +0900

    test blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -5,7 +5,7 @@ import pool from '../utils/connectdb.js';
 
 router.get('/', async function(req, res, next) {
   try {
-    const [result] = await pool.query("SELECT * FROM posts");
+    const [result] = await pool.query("SELECT * FROM Posts");
     res.render('blog', { posts: result });
   } catch(err) {
     next(err);
