3d46cc2ced0486bf07895fe7d5f4ad403d7dfbb8
EthanWonCho <realethanwoncho@gmail.com>
2025-07-03 18:13:05 +0900

    trust proxy

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ dotenv.config();
 
 var app = express();
 
+app.set('trust proxy', 1); // Trust first proxy: Cloudflare
+
 const limiter = rateLimit({
   windowMs: 10 * 1000, // 10 seconds
   max: 30, // limit each IP to 30 requests per windowMs
@@ -30,7 +32,6 @@ const limiter = rateLimit({
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.set('trust proxy', true);
 // logger.token('ip', (req) => req.headers['x-forwarded-for'] || req.connection.remoteAddress);
 // app.use(logger(':ip - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"', {
 //   stream: process.stdout
@@ -45,7 +46,7 @@ app.use(async function (req, res, next) {
   res.on("finish", async () => {
     // Log to the databse like morgan
     try {
-      await pool.query("INSERT INTO RequestLogs (timestamp, ip, method, url, userAgent, referrer, status, contentLength) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [new Date(), req.ip, req.method, req.originalUrl, req.get('user-agent'), req.get('referer') || null, res.statusCode, res.get('content-length') || 0]);
+      await pool.query("INSERT INTO RequestLogs (timestamp, ip, method, url, userAgent, referrer, status, contentLength) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [new Date(), req.ip, req.method, req.originalUrl, req.get('user-agent'), req.get('referer') || nullt , res.statusCode, res.get('content-length') || 0]);
     } catch (err) {
       console.error("Error logging request:", err);
     }
