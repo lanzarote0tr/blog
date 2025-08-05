cc7bbb97f669d58ab81f435a80a8f04df70d6ddd
EthanWonCho <realethanwoncho@gmail.com>
2025-08-05 22:49:02 +0900

    /blog better ejs

--- routes/blog.js
+++ routes/blog.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import helper from '../utils/helper.js';
 var router = express.Router();
 
 import pool from '../utils/connectdb.js';
@@ -6,7 +7,18 @@ import pool from '../utils/connectdb.js';
 router.get('/', async function(req, res, next) {
   try {
     const [result] = await pool.query("SELECT * FROM Posts");
-    res.render('blog', { posts: result });
+    if (result.length === 0) {
+      return res.render('blog', { posts: [], message: 'No posts available.' });
+    }
+    data = [];
+    result.forEach(post => {
+      data.push({
+        PostID: post.PostID || "",
+        Title: post.Title || "",
+        Created_at: helper.formatDate(post.Created_at) || "",
+      });
+    });
+    res.render('blog', { posts: data });
   } catch(err) {
     next(err);
   }
