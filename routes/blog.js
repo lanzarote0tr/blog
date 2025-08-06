892929ad2b3b5eba0180b3deb6b57abf745e472c
EthanWonCho <realethanwoncho@gmail.com>
2025-08-06 22:05:16 +0900

    add post & delete on /blog router

--- routes/blog.js
+++ routes/blog.js
@@ -12,6 +12,20 @@ router.get('/', async function(req, res, next) {
   }
 });
 
+router.post('/createpost', async function(req, res, next) {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    next(createError(400, "Title and content are required."));
+    return;
+  }
+  try {
+    const [result] = await pool.query("INSERT INTO Posts (Title, Content, Author_id) VALUES (?, ?, 1)", [title, content]);
+    res.status(201).json({ message: 'Post created successfully', postId: result.insertId });
+  } catch(err) {
+    next(err);
+  }
+});
+
 router.get('/viewpost', async function(req, res, next) {
   const postId = req.query["id"];
   try {
@@ -20,6 +34,22 @@ router.get('/viewpost', async function(req, res, next) {
       return res.status(404).render('error', { message: 'Post not found', status: 404 });
     }
     res.render('viewpost', { post: result[0] });
+  } catch(err) {
+    next(err);
+  }
+});
+
+router.delete('/', async function(req, res, next) {
+  const postId = req.query.id;
+  if (!postId) {
+    next(createError(400))
+  }
+  try {
+    const [result] = await pool.query("DELETE FROM Posts WHERE PostID = ?", [postId]);
+    if (result.affectedRows === 0) {
+      return res.status(404).render('error', { message: 'Post not found', status: 404 });
+    }
+    res.status(200).json({ message: 'Post deleted successfully' });
   } catch(err) {
     next(err);
   }
